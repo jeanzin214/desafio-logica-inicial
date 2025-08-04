815d8e658017b83e6cfce07a4e1d7b1fd27a6676
Jean Victor Sutil <jeansutil21@gmail.com>
2025-08-03 22:22:53 -0300

    Add files via upload

---
+++ desafio logica 2.js
@@ -0,0 +1,72 @@
+
+//aki estou criando um geraador de numeros aleatorios, para sempre de um rank diferente.
+
+function ranker(wins, defeat) {//aki criei a função que ira fazer o calculo das vitorias e derrotas e dar um saldo que ira definir seu rank
+    let res = wins - defeat
+    const rank = [
+        'iron',
+        'bronze',
+        'silver',
+        'gold',
+        'diamond',
+        'legendary',
+        'immortal'
+    ]
+    if (res <= 0) {//aki criei os valores que voce deve ter para conseguir algum rank
+        return rank[0]
+    } else if (res >= 11 && res <= 20) {
+        return rank[1]
+    } else if (res >= 21 && res <= 50) {
+        return rank[2]
+    } else if (res >= 51 && res <= 80) {
+        return rank[3]
+    } else if (res >= 81 && res <= 90) {
+        return rank[4]
+    } else if (res >= 91 && res <= 100) {
+        return rank[5]
+    } else if (res >= 101) {
+        return rank[6]
+    } else {
+        return "you're noob"//caso voce esteje com apontuaçao negativa ele aparecera esta mensagem.
+    }
+
+}
+
+function namePicker() {//aki criei alguns nome 
+    let name = [
+        'gabriel',
+        'dudu',
+        'lipe',
+        'mauricio',
+        'joao',
+        'jean',
+        'jose',
+        'jessica',
+        'carol',
+        'luanne',
+        'suellen',
+        'priscila',
+        'fabiola',
+        'roberta'
+    ]
+
+    const allNames = Math.floor(Math.random()*name.length);
+    let player= name[allNames]
+        return player
+
+}
+
+function finalMessage(){
+    let wins = Math.floor(Math.random() * 150);
+    let defeat = Math.floor(Math.random() * 130);
+    let player = namePicker();
+    let ranking = ranker(wins, defeat);
+
+    console.log(`Congratulations ${player}! Your current ranking is ${ranking}.`)
+}
+
+for(let i=0 ; i<=3; i++){
+    console.log(`execution ${i}`);
+    finalMessage();
+    console.log('\n')
+}
